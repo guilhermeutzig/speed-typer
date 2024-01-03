b358811ff5ffa7e69b493e8a008042f82e1989d2
Guilherme Utzig <36337530+guilhermeutzig@users.noreply.github.com>
2024-01-03 10:35:18 -0300

    Safe checking private key

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -3,11 +3,15 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
+const PRIVATE_KEY = import.meta.env.PUBLIC_FIREBASE_PRIVATE_KEY
+  ? JSON.parse(import.meta.env.PUBLIC_FIREBASE_PRIVATE_KEY)
+  : undefined;
+
 const serviceAccount = {
   type: "service_account",
   project_id: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
   private_key_id: import.meta.env.PUBLIC_FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.PUBLIC_FIREBASE_PRIVATE_KEY,
+  private_key: PRIVATE_KEY,
   client_email: import.meta.env.PUBLIC_FIREBASE_CLIENT_EMAIL,
   client_id: import.meta.env.PUBLIC_FIREBASE_CLIENT_ID,
   auth_uri: import.meta.env.PUBLIC_FIREBASE_AUTH_URI,
