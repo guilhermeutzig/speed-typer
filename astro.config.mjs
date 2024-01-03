e2fa1c731120e5bd97301ea65f273d0571a2fe0a
Guilherme Utzig <36337530+guilhermeutzig@users.noreply.github.com>
2024-01-03 10:04:02 -0300

    Removed Firebase Hosting; Added Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [solidJs()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
